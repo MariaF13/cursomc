bae4830fed3f52512cb24a7a3315cbf2293330cb
Maria Figueiredo <mf414676@gmail.com>
2022-02-24 15:19:29 -0400

    BD H2 e Criacao automatica da base de dados

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -1,2 +1,55 @@
-package com.nelioalves.cursomc.domain;public class Categoria {
+package com.nelioalves.cursomc.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+@Entity
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    public Categoria(){
+
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Categoria(Integer id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
 }
