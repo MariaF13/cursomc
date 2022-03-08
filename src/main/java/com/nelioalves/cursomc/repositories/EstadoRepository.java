c4d37071f101902277028e5d3527bca9f0e4292f
Maria Figueiredo <mf414676@gmail.com>
2022-03-08 10:12:17 -0400

    Estado e cidade

--- src/main/java/com/nelioalves/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/EstadoRepository.java
@@ -1,9 +1,9 @@
 package com.nelioalves.cursomc.repositories;
 
-import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository <Categoria, Integer> {
+public interface EstadoRepository extends JpaRepository <Estado, Integer> {
 }
